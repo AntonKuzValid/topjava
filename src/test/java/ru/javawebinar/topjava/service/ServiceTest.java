ce00eb7609d66c21b869239a077277776117dd76
AntonKuzValid <antonioplay@mail.ru>
2017-11-18 07:47:17 +0500

    add suit class

---
+++ src/test/java/ru/javawebinar/topjava/service/ServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MealServiceTest.class,UserServiceTest.class})
+public class ServiceTest {
+}
