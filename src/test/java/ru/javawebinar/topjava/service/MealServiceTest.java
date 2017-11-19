b01266ab1bde8ce180647cfbf89009f1eaea1ee4
AntonKuzValid <antonioplay@mail.ru>
2017-11-19 22:53:00 +0500

    ref

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -14,9 +14,13 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -35,58 +39,75 @@ public class MealServiceTest {
 
     @Test
     public void get() throws Exception {
-        Meal meal = service.get(USERS_MEAL_ID, USER_ID);
-        Assert.assertEquals(USER_MEAL, meal);
+        Meal meal = service.get(USER_MEAL_1.getId(), USER_ID);
+        assertMatch(USER_MEAL_1, meal);
     }
 
     @Test(expected = NotFoundException.class)
     public void getOthersPersonMeal() throws Exception {
-        service.get(USERS_MEAL_ID, ADMIN_ID);
+        service.get(USER_MEAL_1.getId(), ADMIN_ID);
     }
 
     @Test(expected = NotFoundException.class)
     public void delete() throws Exception {
-        service.delete(USERS_MEAL_ID, USER_ID);
-        Meal meal = service.get(USERS_MEAL_ID, USER_ID);
+        service.delete(USER_MEAL_1.getId(), USER_ID);
+        service.get(USER_MEAL_1.getId(), USER_ID);
     }
 
     @Test(expected = NotFoundException.class)
     public void deleteOthersPersonMeal() throws Exception {
-        service.delete(USERS_MEAL_ID, ADMIN_ID);
+        service.delete(USER_MEAL_1.getId(), ADMIN_ID);
     }
 
 
     @Test
     public void getBetweenDateTimes() throws Exception {
-        List<Meal> mealList = service.getBetweenDateTimes(
+        List<Meal> mealListActual = service.getBetweenDateTimes(
                 LocalDateTime.of(2017, Month.NOVEMBER, 18, 10, 0),
                 LocalDateTime.of(2017, Month.NOVEMBER, 18, 20, 0),
                 USER_ID);
-        Assert.assertEquals(USER_FILTERED_MEALS, mealList);
+        List<Meal> mealListExpected = Arrays.asList(
+                USER_MEAL_1,
+                USER_MEAL_2,
+                USER_MEAL_3
+        );
+        assertMatch(mealListActual, mealListExpected);
     }
 
     @Test
     public void getAll() throws Exception {
-        List<Meal> mealList = service.getAll(USER_ID);
-        Assert.assertEquals(USER_MEALS, mealList);
+        List<Meal> mealListActual = service.getAll(USER_ID);
+        List<Meal> mealListExpected = Arrays.asList(
+                USER_MEAL_1,
+                USER_MEAL_2,
+                USER_MEAL_3,
+                USER_MEAL_4,
+                USER_MEAL_5,
+                USER_MEAL_6
+        );
+        assertMatch(mealListActual, mealListExpected);
     }
 
     @Test
     public void update() throws Exception {
-        service.update(NEW_MEAL, USER_ID);
-        Meal meal = service.get(USERS_MEAL_ID, USER_ID);
-        Assert.assertEquals(NEW_MEAL, meal);
+        Meal newMeal = new Meal(USER_MEAL_1);
+        newMeal.setDescription("new Meal");
+        newMeal.setCalories(100);
+        service.update(newMeal, USER_ID);
+        Meal meal = service.get(USER_MEAL_1.getId(), USER_ID);
+        assertMatch(newMeal, meal);
     }
 
     @Test(expected = NotFoundException.class)
     public void updateOthersPersonMeal() throws Exception {
-        service.update(NEW_MEAL, ADMIN_ID);
+        service.update(USER_MEAL_1, ADMIN_ID);
     }
 
     @Test
     public void create() throws Exception {
-        service.create(new Meal(LocalDateTime.of(2010, Month.MAY, 10, 10, 00), "new Meal", 100), USER_ID);
-        Assert.assertEquals(NEW_USER_MEALS, service.getAll(USER_ID));
+        Meal meal = new Meal(LocalDateTime.of(2010, Month.MAY, 10, 10, 00), "new Meal", 100);
+        Meal newMeal = service.create(meal, USER_ID);
+        assertMatch(newMeal, service.get(newMeal.getId(), USER_ID));
     }
 
 }
