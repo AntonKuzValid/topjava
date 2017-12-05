a542478c74a61e383e807c9137458d28a3ac192c
AntonKuzValid <antonioplay@mail.ru>
2017-12-05 08:19:05 +0300

    fix jdbcmealrepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHSQLDBImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHSQLDBImpl.java
@@ -1,25 +1,14 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
-import com.sun.jmx.snmp.Timestamp;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
-import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import javax.sql.DataSource;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
 
 @Profile(value = Profiles.JDBC)
 @Repository
@@ -29,34 +18,7 @@ public class JdbcMealRepositoryHSQLDBImpl extends AbstractMealRepository {
     }
 
     @Override
-    public Meal save(Meal meal, int userId) {
-        MapSqlParameterSource map = new MapSqlParameterSource()
-                .addValue("id", meal.getId())
-                .addValue("description", meal.getDescription())
-                .addValue("calories", meal.getCalories())
-                .addValue("date_time", DateTimeUtil.convertToTimeStamp(meal.getDateTime()))
-                .addValue("user_id", userId);
-
-
-        if (meal.isNew()) {
-            Number newId = insertMeal.executeAndReturnKey(map);
-            meal.setId(newId.intValue());
-        } else {
-            if (namedParameterJdbcTemplate.update("" +
-                            "UPDATE meals " +
-                            "   SET description=:description, calories=:calories, date_time=:date_time " +
-                            " WHERE id=:id AND user_id=:user_id"
-                    , map) == 0) {
-                return null;
-            }
-        }
-        return meal;
-    }
-
-    @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId,  DateTimeUtil.convertToTimeStamp(startDate),  DateTimeUtil.convertToTimeStamp(endDate));
+    protected Timestamp convert(LocalDateTime localDateTime) {
+        return Timestamp.valueOf(localDateTime);
     }
 }
