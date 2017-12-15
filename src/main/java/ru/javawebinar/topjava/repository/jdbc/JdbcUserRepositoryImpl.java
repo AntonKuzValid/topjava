19ca38df3fa898eb41393169fff8e0137a6c428b
AntonKuzValid <antonioplay@mail.ru>
2017-12-15 14:37:54 +0300

    do HW_06 optional

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -8,16 +8,20 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
+@Transactional(readOnly = true)
 @Repository
 public class JdbcUserRepositoryImpl implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+    private static final JDBCUserMapper userMapper = new JDBCUserMapper();
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -36,12 +40,20 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
+//        UserSqlParameterSource parameterSource= new UserSqlParameterSource(user);
+//        parameterSource.addValue("roles", user.getRoles());
 
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
+//            user.getRoles().forEach(r->jdbcTemplate.update("INSERT  INTO user_roles (user_id, role) VALUES (?,?)",newKey.intValue(), r.toString()));
+
+            List <Object[]> batch=new ArrayList<>();
+            user.getRoles().forEach(r->batch.add(new Object[]{newKey.intValue(),r.toString()}));
+            jdbcTemplate.batchUpdate("INSERT  INTO user_roles (user_id, role) VALUES (?,?)",batch);
         } else if (namedParameterJdbcTemplate.update(
                 "UPDATE users SET name=:name, email=:email, password=:password, " +
                         "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
@@ -51,25 +63,27 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = jdbcTemplate.query("SELECT id, email, NAME, ENABLED, USER_ROLES.ROLE, CALORIES_PER_DAY, PASSWORD, REGISTERED FROM users LEFT OUTER JOIN user_roles ON user_roles.user_id=users.id WHERE id=?", userMapper, id);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        List<User> users = jdbcTemplate.query("SELECT id, email, NAME, ENABLED, USER_ROLES.ROLE, CALORIES_PER_DAY, PASSWORD, REGISTERED FROM users LEFT OUTER JOIN user_roles ON user_roles.user_id=users.id WHERE email=?", userMapper, email);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return jdbcTemplate.query("SELECT id, email, NAME, ENABLED, USER_ROLES.ROLE, CALORIES_PER_DAY, PASSWORD, REGISTERED FROM users LEFT OUTER JOIN user_roles ON user_roles.user_id=users.id ORDER BY name, email",userMapper.getUserResultSetExtractor());
+
     }
 }
