a2fe9743169acf5b3774cb7b8f794825d4b7db84
AntonKuzValid <antonioplay@mail.ru>
2017-12-22 18:22:27 +0300

    Do MealRestController

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ResourceControllerTest extends AbstractControllerTest{
+public class ResourceControllerTest extends AbstractControllerTest {
     @Test
     public void testCSS() throws Exception {
         mockMvc.perform(get("/"))
